f4a6a8fe4a08558e72545c05dd3e5224e71570b6
ST10469292-Graham-Foley <ST10469292@vcconnect.edu.za>
2025-04-16 09:54:26 +0200

    Created start page UI

--- app/src/main/java/com/example/starwarsquiz/MainActivity.kt
+++ app/src/main/java/com/example/starwarsquiz/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.starwarsquiz
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
