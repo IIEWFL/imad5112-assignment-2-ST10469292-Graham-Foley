70c5ffd6bff46b883f7abc74e7819887f96d56ce
ST10469292-Graham-Foley <ST10469292@vcconnect.edu.za>
2025-04-16 11:28:41 +0200

    Added lateinit variables

--- app/src/main/java/com/example/starwarsquiz/Quiz.kt
+++ app/src/main/java/com/example/starwarsquiz/Quiz.kt
@@ -1,6 +1,8 @@
 package com.example.starwarsquiz
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,12 @@ class Quiz : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    private lateinit var questionTxt: TextView
+    private lateinit var trueBtn: Button
+    private lateinit var falseBtn: Button
+    private lateinit var resultTxt: TextView
+    private lateinit var nextBtn: Button
+    private lateinit var exitBtn: Button
     }
 }
