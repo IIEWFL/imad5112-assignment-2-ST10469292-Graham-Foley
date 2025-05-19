8c11a58d615402fb83ee59bd67d2e8f6db2a267d
ST10469292-Graham-Foley <ST10469292@vcconnect.edu.za>
2025-05-19 17:55:53 +0200

    Coded reviewTxt (Not Working)

--- app/src/main/java/com/example/starwarsquiz/Review.kt
+++ app/src/main/java/com/example/starwarsquiz/Review.kt
@@ -1,6 +1,7 @@
 package com.example.starwarsquiz
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,22 @@ class Review : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val reviewTxt = findViewById<TextView>(R.id.reviewTxt)
+
+        val questions = intent.getStringArrayExtra("questions")
+        val answers = intent.getBooleanArrayExtra("answers")
+
+        val reviewText = StringBuilder()
+        if (questions != null && answers != null && questions.size == answers.size) {
+            for (i in questions.indices) {
+                reviewText.append("${i + 1}. ${questions[i]}\n")
+                reviewText.append("Answer: ${if (answers[i]) "True" else "False"}\n\n")
+            }
+            reviewTxt.text = reviewText.toString()
+        }
+        else {
+            reviewTxt.text = "Error loading review data. $questions"
         }
     }
 }
